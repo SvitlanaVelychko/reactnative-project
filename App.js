2b9543be979abe96bf2334417716731d21eae93b
SvitlanaVelychko <svel2608@gmail.com>
2022-12-14 00:40:02 +0200

    Add styles in registration form

--- App.js
+++ App.js
@@ -3,37 +3,46 @@ import {
   StyleSheet,
   Text,
   View,
+  Image,
   ImageBackground,
   TextInput,
   TouchableOpacity,
+  Platform,
 } from 'react-native';
 
 export default function App() {
+  console.log(Platform.OS)
   return (
     <View style={styles.container}>
       <ImageBackground
         source={require("./assets/images/bg.jpg")}
         style={styles.image}>
         <View style={styles.formContainer}>
-          <Text style={styles.text}>Реєстрація</Text>
+          <View style={styles.userAvatar}>
+            <Image
+              source={require("./assets/images/add.png")}
+              style={styles.addBtn}
+            />
+          </View>
+          <Text style={styles.text}>Регистрация</Text>
           <View style={styles.form}>
-            <View>
+            <View style={{marginBottom: 16}}>
               <TextInput
                 style={styles.input}
                 textAlign={'left'}
-                placeholder={"Логін"}
+                placeholder={"Логин"}
                 placeholderTextColor={"#BDBDBD"}
               />
             </View>
-            <View>
+            <View style={{marginBottom: 16}}>
               <TextInput
                 style={styles.input}
                 textAlign={'left'}
-                placeholder={"Адреса електронної пошти"}
+                placeholder={"Адрес електронной почты"}
                 placeholderTextColor={"#BDBDBD"}
               />
             </View>
-            <View style={{marginBottom: 43}}>
+            <View style={{marginBottom: 43, position: "relative"}}>
               <TextInput
                 style={styles.input}
                 textAlign={'left'}
@@ -41,9 +50,15 @@ export default function App() {
                 placeholderTextColor={"#BDBDBD"}
                 secureTextEntry={true}
               />
+              <TouchableOpacity activeOpacity={0.8} style={styles.passwordLink}>
+              <Text style={styles.linkText}>Показать</Text>
+            </TouchableOpacity>
             </View>
             <TouchableOpacity activeOpacity={0.8} style={styles.btn}>
-              <Text style={styles.btnTitle}>Зареєструватися</Text>
+              <Text style={styles.btnTitle}>Зарегистрироваться</Text>
+            </TouchableOpacity>
+            <TouchableOpacity activeOpacity={0.8} style={styles.link}>
+              <Text style={styles.linkText}>Уже есть аккаунт? Войти</Text>
             </TouchableOpacity>
           </View>
         </View>
@@ -65,17 +80,32 @@ const styles = StyleSheet.create({
     justifyContent: "flex-end",
   },
   formContainer: {
-    height: 549,
+    position: "relative",
+    height: 550,
     backgroundColor: "#FFFFFF",
     borderTopLeftRadius: 25,
     borderTopRightRadius: 25,
+  },
+  userAvatar: {
+    position: "absolute",
+    top: -60,
+    alignSelf: "center",
+    width: 120,
+    height: 120,
+    borderRadius: 16,
+    backgroundColor: "#F6F6F6",
+  },
+  addBtn: {
+    position: "absolute",
+    bottom: 15,
+    right: -12.5,
   },
   form: {
     marginHorizontal: 16,
   },
   text: {
     marginBottom: 32,
-    marginTop: 32,
+    marginTop: 92,
     marginLeft: "auto",
     marginRight: "auto",
     fontSize: 30,
@@ -84,7 +114,6 @@ const styles = StyleSheet.create({
   },
   input: {
     paddingLeft: 16,
-    marginBottom: 16,
     height: 50,
     borderWidth: 1,
     borderColor: "#E8E8E8",
@@ -94,13 +123,16 @@ const styles = StyleSheet.create({
     fontSize: 16,
     lineHeight: 1.19,
   },
+  passwordLink: {
+    position: "absolute",
+    right: 16,
+    top: 16,
+  },
   btn: {
-    // flex: 1,
-    alignContent: "center",
+    height: 50,
+    alignItems: "center",
     justifyContent: "center",
     marginBottom: 16,
-    paddingTop: 16,
-    paddingBottom: 16,
     backgroundColor: "#FF6C00",
     borderRadius: 100,
   },
@@ -108,4 +140,11 @@ const styles = StyleSheet.create({
     fontSize: 16,
     color: "#FFFFFF",
   },
+  link: {
+    alignItems: "center",
+  },
+  linkText: {
+    fontSize: 16,
+    color: "#1B4371",
+  }
 });
